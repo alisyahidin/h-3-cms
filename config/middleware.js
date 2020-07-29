6021195b866ade6a650f41b09d39e98c59562212
alisyahidin <ali.syahidin@sg-dsa.com>
2020-07-29 22:17:36 +0700

    add cache

--- config/middleware.js
+++ config/middleware.js
@@ -6,6 +6,16 @@ module.exports = {
       "formidable": {
         "maxFileSize": 1048576000
       }
-    }
+    },
+    cache: {
+      enabled: true,
+      models: [
+        'awards-page',
+        'footer',
+        'landing-page',
+        'logo',
+        'our-works-page',
+      ],
+    },
   }
 }
