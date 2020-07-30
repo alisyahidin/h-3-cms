df4f1173dc2698fc8bea1788fb88efe9a58538b5
alisyahidin <ali.syahidin@sg-dsa.com>
2020-07-30 11:17:59 +0700

    update build

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: 'EmFnbRLlClH6xqPzh7R+yzVNRmkDO4gg+18yDrM7fANHCumrU+bnHoet5Vi5xYjib55Zpj56o34unRwRnrUcwQ=='
+    }
+  }
 });
